53eeedaa4f400c6aa36a9baf9e977f7182fd21ee
camziny <cameronziny@gmail.com>
2022-05-16 16:47:37 -0400

    -add Team Serializer. -add Player serializer. -add serializing to Teams Router. -add Serializing to Players Router.

--- server/src/routes/api/v1/teamsRouter.js
+++ server/src/routes/api/v1/teamsRouter.js
@@ -1,18 +1,32 @@
-import express from "express"
-import objection from "objection"
+import express from "express";
+import objection from "objection";
+import TeamSerializer from "../../../serializers/TeamSerializer.js";
 
-import { Team } from "../../../models/index.js"
+import { Team } from "../../../models/index.js";
 
-const teamsRouter = new express.Router()
+const teamsRouter = new express.Router();
 
 teamsRouter.get("/", async (req, res) => {
-    try {
-        const teams = await Team.query()
-        return res.status(200).json({ teams: teams })
-    } catch (error) {
-        return res.status(500).json({ errors: error })
-    }
-})
+  try {
+    const teams = await Team.query();
+    const serializedTeams = await Promise.all(
+      teams.map(async (team) => await TeamSerializer.getSummary(team))
+    );
+    return res.status(200).json({ teams: serializedTeams });
+  } catch (error) {
+    return res.status(500).json({ errors: error });
+  }
+});
 
-export default teamsRouter
+teamsRouter.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const team = await Team.query().findById(id);
+    const serializedTeam = await serializedTeam.getDetails(team);
+    return res.status(200).json({ team: serializedTeam });
+  } catch (error) {
+    return res.status(500).json({ errors: error });
+  }
+});
 
+export default teamsRouter;

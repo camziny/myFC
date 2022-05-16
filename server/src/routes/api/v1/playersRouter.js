bd85adc38c11ad409d207b65e32f01ae547b8131
camziny <cameronziny@gmail.com>
2022-05-16 16:20:41 -0400

    -add get by id to players router. -add get by id to teams router.

--- server/src/routes/api/v1/playersRouter.js
+++ server/src/routes/api/v1/playersRouter.js
@@ -1,16 +1,26 @@
-import express from "express"
-import objection from "objection"
-import { Player } from "../../../models"
+import express from "express";
+import objection from "objection";
+import { Player } from "../../../models";
 
-const playersRouter = new express.Router()
+const playersRouter = new express.Router();
 
 playersRouter.get("/", async (req, res) => {
-    try {
-        const players = await Player.query()
-        return res.status(200).json({ players: players })
-    } catch (error) {
-            return res.status(500).json({ errors: error})
-        }
-    })
+  try {
+    const players = await Player.query();
+    return res.status(200).json({ players: players });
+  } catch (error) {
+    return res.status(500).json({ errors: error });
+  }
+});
 
-    export default playersRouter
+playersRouter.get("/:id", async (req, res) => {
+  try {
+    const player = await Player.query().findById(req.params.id);
+    player.teams = await player.$relatedQuery("teams");
+    return res.status(200).json({ player: player });
+  } catch (error) {
+    return res.status(500).json({ errors: error });
+  }
+});
+
+export default playersRouter;
--- server/src/routes/api/v1/teamsRouter.js
+++ server/src/routes/api/v1/teamsRouter.js
@@ -1,18 +1,27 @@
-import express from "express"
-import objection from "objection"
+import express from "express";
+import objection from "objection";
 
-import { Team } from "../../../models/index.js"
+import { Team } from "../../../models/index.js";
 
-const teamsRouter = new express.Router()
+const teamsRouter = new express.Router();
 
 teamsRouter.get("/", async (req, res) => {
-    try {
-        const teams = await Team.query()
-        return res.status(200).json({ teams: teams })
-    } catch (error) {
-        return res.status(500).json({ errors: error })
-    }
-})
+  try {
+    const teams = await Team.query();
+    return res.status(200).json({ teams: teams });
+  } catch (error) {
+    return res.status(500).json({ errors: error });
+  }
+});
 
-export default teamsRouter
+teamsRouter.get("/:id", async (req, res) => {
+  try {
+    const team = await Team.query().findById(req.params.id);
+    team.players = await team.$relatedQuery("players");
+    return res.status(200).json({ team: team });
+  } catch (error) {
+    return res.status(500).json({ errors: error });
+  }
+});
 
+export default teamsRouter;
